2b415e12a8f0c34e398e12865a3f70521c3d43a5
fabian0702 <48295869+fabian0702@users.noreply.github.com>
2026-02-12 21:14:34 +0100

    remove manager component

--- manager/c2/manager/base.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from secrets import token_hex
-
-class Message(BaseModel):
-    operation: str
-    data: dict
-    id:str = Field(default_factory=lambda : token_hex(16))
